278149379aca25f03b13b5e795d1cc0846c3a61d
Keijiro Takahashi <keijiro@gmail.com>
2026-02-21 23:10:03 +0900

    Refactor DigitalGlitch

--- Packages/jp.keijiro.kino-glitch/Runtime/DigitalGlitch.shader
+++ Packages/jp.keijiro.kino-glitch/Runtime/DigitalGlitch.shader
@@ -20,24 +20,29 @@ float4 Frag(Varyings input) : SV_Target
 {
     float2 uv = input.texcoord;
 
+    // Block selection
     int2 block = (int2)(uv * float2(_BlockCols, _BlockRows));
     float nx = (block.x + block.y * _BlockCols + 0.5) * _NoiseTex_TexelSize.x;
 
+    // Noise sample
     float4 glitch = SAMPLE_TEXTURE2D_X(_NoiseTex, sampler_NoiseTex, float2(nx, 0.5));
+    float4 glitch2 = glitch * glitch;
 
-    float wD = step(_Threshold, pow(glitch.z, 2.5));
-    float wF = step(_Threshold, pow(glitch.w, 2.5));
-    float wC = step(_Threshold, pow(glitch.z, 3.5));
+    // Displacement
+    float2 uv2 = _Threshold < glitch2.z ? frac(uv + glitch.xy) : uv;
 
-    float2 duv = frac(uv + glitch.xy * wD);
-    float4 source = SAMPLE_TEXTURE2D_X(_BlitTexture, sampler_LinearClamp, duv);
+    // Color source samples
+    float4 src_curr = SAMPLE_TEXTURE2D_X(_BlitTexture, sampler_LinearClamp, uv2);
+    float4 src_prev = SAMPLE_TEXTURE2D_X(_TrashTex, sampler_LinearClamp, uv2);
 
-    float3 color = lerp(source, SAMPLE_TEXTURE2D_X(_TrashTex, sampler_TrashTex, duv), wF).rgb;
+    // Sample selection
+    float3 color = _Threshold < glitch2.w ? src_prev : src_curr;
 
+    // Negate
     float3 neg = saturate(color.grb + (1 - dot(color, float3(1, 1, 1))) * 0.5);
-    color = lerp(color, neg, wC);
+    color = _Threshold < glitch2.z ? neg : color;
 
-    return float4(color, source.a);
+    return float4(color, src_curr.a);
 }
 
 ENDHLSL
--- Packages/jp.keijiro.kino-glitch/Runtime/DigitalGlitchNoiseTexture.cs
+++ Packages/jp.keijiro.kino-glitch/Runtime/DigitalGlitchNoiseTexture.cs
@@ -11,55 +11,23 @@ namespace KinoGlitch {
 [BurstCompile]
 sealed class DigitalGlitchNoiseTexture : IDisposable
 {
+    //
+    // Public interface
+    //
+
     public const int TextureWidth = 2048;
 
     public Texture2D Texture { get; private set; }
-    NativeArray<Color32> _pixels;
-
-    static Color32 NextColor(ref Unity.Mathematics.Random random)
-      => new
-      (
-          (byte)random.NextInt(256),
-          (byte)random.NextInt(256),
-          (byte)random.NextInt(256),
-          (byte)random.NextInt(256)
-      );
-
-    [BurstCompile]
-    static void UpdateNoise(ref NativeArray<Color32> pixels, uint seed)
-    {
-        var random = new Unity.Mathematics.Random(seed);
-        for (var j = 0; j < 32; j++)
-        {
-            var start = random.NextInt(0, pixels.Length);
-            var length = random.NextInt(1, 3) + (int)(math.pow(random.NextFloat(), 10) * 64);
-            var color = NextColor(ref random);
-
-            for (var i = start; i < math.min(start + length, pixels.Length); i++)
-            {
-                pixels[i] = color;
-            }
-        }
-    }
 
     public DigitalGlitchNoiseTexture()
     {
-        Texture = new Texture2D(TextureWidth, 1, TextureFormat.ARGB32, false)
+        Texture = new Texture2D(TextureWidth, 1, TextureFormat.ARGB32, false, true)
         {
             wrapMode = TextureWrapMode.Repeat,
             filterMode = FilterMode.Point
         };
-        _pixels = new NativeArray<Color32>(TextureWidth, Allocator.Persistent);
+        _pixels = new NativeArray<uint>(TextureWidth, Allocator.Persistent);
         Update();
-    }
-
-    public void Update()
-    {
-        var seed = (uint)Random.Range(1, int.MaxValue);
-        UpdateNoise(ref _pixels, seed);
-
-        Texture.LoadRawTextureData(_pixels);
-        Texture.Apply();
     }
 
     public void Dispose()
@@ -67,6 +35,34 @@ sealed class DigitalGlitchNoiseTexture : IDisposable
         CoreUtils.Destroy(Texture);
         Texture = null;
         if (_pixels.IsCreated) _pixels.Dispose();
+    }
+
+    public void Update()
+    {
+        UpdateNoise(ref _pixels, (uint)Random.Range(1, int.MaxValue));
+        Texture.LoadRawTextureData(_pixels);
+        Texture.Apply();
+    }
+
+    //
+    // Private members
+    //
+
+    NativeArray<uint> _pixels;
+
+    [BurstCompile]
+    static void UpdateNoise(ref NativeArray<uint> pixels, uint seed)
+    {
+        var random = new Unity.Mathematics.Random(seed);
+        for (var j = 0; j < 32; j++)
+        {
+            var start = random.NextInt(0, pixels.Length);
+            var span1 = random.NextInt(1, 3);
+            var span2 = (int)(math.pow(random.NextFloat(), 16) * 100);
+            var end = math.min(start + span1 + span2, pixels.Length);
+            var color = random.NextUInt();
+            for (var i = start; i < end; i++) pixels[i] = color;
+        }
     }
 }
 
